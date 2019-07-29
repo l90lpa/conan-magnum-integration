b84b57854cb54a1b1218fa40a5cd9b8a40f97d92
Devin Schwab <digidevin@gmail.com>
2019-07-29 11:06:58 -0700

    :heavy_plus_sign: Changed deps to stable channel

--- conanfile.py
+++ conanfile.py
@@ -64,8 +64,8 @@ class MagnumIntegrationConan(ConanFile):
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
-    requires = ("magnum/2019.01@rhololkeolke/testing",
-                "corrade/2019.01@rhololkeolke/testing")
+    requires = ("magnum/2019.01@rhololkeolke/stable",
+                "corrade/2019.01@rhololkeolke/stable")
 
     def config_options(self):
         if self.settings.os == 'Windows':
