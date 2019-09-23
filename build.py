97ac3c6bb24e01cda184ddbef627cb9e0caf78c5
Devin Schwab <digidevin@gmail.com>
2019-09-23 12:12:07 -0400

    :construction_worker: Changed CI to use docker images and named builds

--- build.py
+++ build.py
@@ -1,48 +1,59 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import copy
+from collections import defaultdict
 
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add(
-        settings={"arch": "x86_64", "build_type": "Release"},
-        options={
-            "corrade:with_rc": True,
-            "magnum:with_sdl2application": False,
-            "magnum:with_glfwapplication": True,
-            "magnum:with_tgaimporter": True,
-            "magnum:with_anysceneimporter": True,
-            "magnum:with_meshtools": True,
-            "magnum-integration:with_imgui": True,
-            "magnum-integration:with_bullet": True,
-            "bullet3:double_precision": True,
-        },
-    )
-    builder.add(
-        settings={"arch": "x86_64", "build_type": "Debug"},
-        options={
-            "corrade:with_rc": True,
-            "magnum:with_sdl2application": False,
-            "magnum:with_glfwapplication": True,
-            "magnum:with_tgaimporter": True,
-            "magnum:with_anysceneimporter": True,
-            "magnum:with_meshtools": True,
-            "magnum-integration:with_imgui": True,
-            "magnum-integration:with_bullet": True,
-            "bullet3:double_precision": True,
-        },
-    )
+    builder.add_common_builds()
 
     # add c++17 build configs
-    new_builds = []
+    named_builds = defaultdict(list)
     for settings, options, env_vars, build_requires, reference in builder.items:
-        new_settings = copy.copy(settings)
-        new_settings["compiler.cppstd"] = "17"
-        new_settings["compiler.libcxx"] = "libstdc++11"
-        new_builds.append([settings, options, env_vars, build_requires])
-        new_builds.append([new_settings, options, env_vars, build_requires])
-    builder.builds = new_builds
+        settings["compiler.cppstd"] = "17"
+        settings["compiler.libcxx"] = "libstdc++11"
+
+        # magnum non-default settings
+        options["magnum:with_sdl2application"] = False
+        options["magnum:with_glfwapplication"] = True
+        options["magnum:with_tgaimporter"] = True
+        options["magnum:with_anysceneimporter"] = True
+        options["magnum:with_meshtools"] = True
+
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'shared' if options['magnum-integration:shared'] else 'static'}"
+        ].append([settings, options, env_vars, build_requires, reference])
+
+        imgui_options = copy.copy(options)
+        imgui_options["magnum-integration:with_imgui"] = True
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'shared' if options['magnum-integration:shared'] else 'static'}"
+            "-imgui"
+        ].append([settings, imgui_options, env_vars, build_requires, reference])
+
+        eigen_options = copy.copy(options)
+        eigen_options["magnum-integration:with_eigen"] = True
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'shared' if options['magnum-integration:shared'] else 'static'}"
+            "-eigen"
+        ].append([settings, eigen_options, env_vars, build_requires, reference])
+
+        imgui_and_eigen_options = copy.copy(options)
+        imgui_and_eigen_options["magnum-integration:with_imgui"] = True
+        imgui_and_eigen_options["magnum-integration:with_eigen"] = True
+        named_builds[
+            f"{settings['build_type']}-"
+            f"{'shared' if options['magnum-integration:shared'] else 'static'}"
+            "-imgui-eigen"
+        ].append(
+            [settings, imgui_and_eigen_options, env_vars, build_requires, reference]
+        )
+
+    builder.builds = []
+    builder.named_builds = named_builds
 
     builder.run()
